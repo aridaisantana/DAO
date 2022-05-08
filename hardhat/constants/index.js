46f610c6fbfb35bc8f48429f23494c9dadb465a5
aridaisantana <aridaitecnologia@gmail.com>
2022-05-08 17:30:40 +0100

    FEATURE: Add DAO Smart Contract

---
+++ hardhat/constants/index.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0xE1052375974ad1099911e8124a9318b8541E75f0";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
